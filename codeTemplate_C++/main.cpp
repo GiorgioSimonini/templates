cec073e971e51c8bcdd0eaf7ce95fcc83ccd9589
GiorgioSimonini <g.simonini1@studenti.unipi.it>
2021-02-17 13:06:58 +0100

    init templates

---
+++ codeTemplate_C++/main.cpp
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------------------------------------------*
+	Author: Giorgio Simonini
+	Title:
+    Data:
+	Description:
+	Functionalities:
+    To do:
+    Problems:
+
+	Exec on a single processor: 		sudo taskset --cpu-list 'numProc' ./'file'    
+                                        (example)    sudo taskset --cpu-list 2 ./main
+	Scheduler linux and kernel:   	    less /etc/security/limits.conf 
+
+------------------------------------------------------------------------------------------------------------------*/
+#include <iostream>
+#include "global.h"
+using namespace std;
+/*----------*
+ * COSTANTS *
+ *----------*/
+
+
+/*-----------*
+ * VARIABLES *
+ *-----------*/
+
+
+/*------------*
+ * STRUCTURES *
+ *------------*/
+
+
+/*---------------------*
+ * FUNCTION PROTOTYPES *
+ *---------------------*/
+
+
+/*----------------------*
+ * FUNCTION DEFINITIONS *
+ *----------------------*/
+
+
+/*------*
+ * MAIN *
+ *------*/
+int main(void* args){
+	
+	return 0;
+}
